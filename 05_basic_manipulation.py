8790db1063d260fde403fe497a46fa97bd14061d
Rajorshi Mukherjee <mr.rajorshi.mukherjee@gmail.com>
2025-05-07 20:16:28 +0530

    Syncing changes

--- 05_basic_manipulation.py
+++ 05_basic_manipulation.py
@@ -2,7 +2,7 @@ import cv2 as cv
 
 # Convert image to grayscale
 
-img = cv.imread('Images/parrot.jpg')
+img = cv.imread('Images/parrot_smol.jpg')
 cv.imshow('Original', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -10,4 +10,7 @@ cv.imshow('Gray scale', gray)
 
 # Blur image
 
+# blur = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT)
+blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
 cv.waitKey(0)
